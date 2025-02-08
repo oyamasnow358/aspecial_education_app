b4bdc6db639bc491b2866d950e115b57a36c133b
oyamasnow358 <oyamayuuji596308@gmail.com>
2025-02-08 13:24:32 +0900

    A

--- tokusi_app.py
+++ tokusi_app.py
@@ -794,5 +794,6 @@ if menu == "指導支援内容":
     # **別のWebアプリへのリンク**
     st.markdown("関連Webアプリに移動する")
     st.markdown("[発達段階能力チャート作成](https://specialexcel2apppy-bo6jrng9gyqw5dmfcgwbl5.streamlit.app/)")
+    st.markdown("[特別支援教育サポート分析法・心療法](https://bunnsekiapppy-6zctfql94fk2x3ghmu5pmx.streamlit.app//)")
 
    
