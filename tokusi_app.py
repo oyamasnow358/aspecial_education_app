3efcd9ef509f700339b0fd62ffeb2716043382dd
oyamasnow358 <oyamayuuji596308@gmail.com>
2025-03-23 22:28:02 +0900

    W

--- tokusi_app.py
+++ tokusi_app.py
@@ -1594,11 +1594,6 @@ elif st.session_state.page == "analysis":
 
  # **区切り線**
 st.markdown("---")
-# **別のWebアプリへのリンク**
-st.markdown("🌎関連Webアプリに移動する")
-st.markdown("[発達段階能力チャート作成](https://specialexcel2apppy-bo6jrng9gyqw5dmfcgwbl5.streamlit.app/)")
-st.markdown("[特別支援教育サポート分析法・心療法](https://bunnsekiapppy-6zctfql94fk2x3ghmu5pmx.streamlit.app/)")
-st.markdown("---")  # 区切り線  
 st.markdown("📁教育・心理分析ツール") 
 st.markdown("[応用行動分析](https://abaapppy-k7um2qki5kggexf8qkfxjc.streamlit.app/)")
 st.markdown("[機能的行動評価分析](https://kinoukoudou-ptfpnkq3uqgaorabcyzgf2.streamlit.app/)") 
